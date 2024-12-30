7bd7fa618df588d73fe550c347ad0ff3bbb9261a
ashkar2023 <muhammedashkar2023@gmail.com>
2024-12-30 11:59:18 +0530

    modified: css variables & tailwind config
    modified css variables to rgb value numbers only
    format rgb(1,2,3) -> 23 24 255
    
    modified tailwind config custom theme color values to handle rgb value with opacity modifier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,26 +13,30 @@ export default {
                 sans: ["Titillium", 'sans-serif']
             },
             colors: {
-                "app-primary": "var(--app-primary)",
-                "app-secondary": "var(--app-secondary)",
-                "app-tertiary": "var(--app-tertiary)",
-                "app-t-primary": "var(--app-text-primary)",
-                "app-t-secondary": "var(--app-text-secondary)",
-                "app-accent": "var(--ct-accent)"
-            }
-            ,
+                "app-primary": "rgb(var(--app-primary) / <alpha-value>)",
+                "app-secondary": "rgb(var(--app-secondary) / <alpha-value>)",
+                "app-tertiary": "rgb(var(--app-tertiary) / <alpha-value>)",
+                "app-t-primary": "rgb(var(--app-text-primary) / <alpha-value>)",
+                "app-t-secondary": "rgb(var(--app-text-secondary) / <alpha-value>)",
+                "app-accent": "rgb(var(--app-accent) / <alpha-value>)" // this works fine
+            },
             keyframes: {
                 slideDown: {
                     "0%": { opacity: '0', transform: 'translateY(-10px)' },
                     "100%": { opacity: '1', transform: 'translateY(0)' }
-                }
+                },
+                "slide-in-fl": {
+                    "0%": { opacity: '0', transform: 'translateX(-200px)' },
+                    "100%": { opacity: '1', transform: 'translateY(0)' }
+                },
             },
             animation: {
                 slideDown: 'slideDown 0.2s ease-in',
+                "slide-in-fl": "slide-in-fl 0.2s ease-in-out",
             },
             screens: {
                 "mobile": "480px"
-            }
+            },
         },
     },
     plugins: [nextui(),],
